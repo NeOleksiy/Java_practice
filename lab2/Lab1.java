460493df99512ef17696c5d365d03242431e30da
NeOleksiy <92272791+NeOleksiy@users.noreply.github.com>
2022-10-11 14:05:52 +0300

    Update Lab1.java

--- lab2/Lab1.java
+++ lab2/Lab1.java
@@ -6,11 +6,20 @@ public class Lab1 extends Point3d {
         double d1 = Double.parseDouble(args[0]);
         double d2 = Double.parseDouble(args[1]);
         double d3 = Double.parseDouble(args[2]);
-        Point3d myPoint = new Point3d(d1,d2,d3);
-        if (myPoint.compare(d1,d2,d3))
-            System.out.println(computeArea(d1,d2,d3));
+        double d4 = Double.parseDouble(args[3]);
+        double d5 = Double.parseDouble(args[4]);
+        double d6 = Double.parseDouble(args[5]);
+        double d7 = Double.parseDouble(args[6]);
+        double d8 = Double.parseDouble(args[7]);
+        double d9 = Double.parseDouble(args[8]);
+        Point3d Point1 = new Point3d(d1,d2,d3);
+        Point3d Point2 = new Point3d(d4,d5,d6);
+        Point3d Point2 = new Point3d(d7,d8,d9);
+        if (Point3d.compare(Point1,Point2) || Point3d.compare(Point1,Point3) || Point3d.compare(Point3,Point2))
+           System.out.println("there are 2 equal points");
         else
-            System.out.println("there are 2 equal points");
+            System.out.println(computeArea(Point1.distanceTo(Point1,Point2), Point2.distanceTo(Point2,Point3), Point1.distanceTo(Point1, Point3));
+            
     }
     //Расчёт площади по формуле Герона
     public static double computeArea(double num1, double num2, double num3) {
