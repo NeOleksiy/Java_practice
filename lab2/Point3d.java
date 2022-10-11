e872517ac7ff95a8450b62a9ce3c397a21b6e240
NeOleksiy <92272791+NeOleksiy@users.noreply.github.com>
2022-10-11 13:48:57 +0300

    Update Point3d.java

--- lab2/Point3d.java
+++ lab2/Point3d.java
@@ -1,50 +1,32 @@
 package lab2;
 
-public class Point3d {
+public class Point3d extends Point2d{
     //Поля для координат
-    private double xCoord;
-    private double yCoord;
+    
     private double zCoord;
 
     //Конструктор инициализации
     public Point3d (double x , double y, double z) {
-        xCoord = x ;
-        yCoord = y ;
+        super(x,y);
         zCoord = z ;
     }
-    //Конструктор по умолчанию
-    public Point3d () {}
+   
 
     //Методы возвращение значения
-    public double getX(Point3d coord2) {
-        return xCoord;
-    }
-
-    public double getY(Point3d coord2) {
-        return yCoord;
-    }
+   
 
     public double getZ(Point3d coord1) {
         return zCoord;
     }
     //Методы установки значения
-    public void setX(double value) {
-        xCoord = value;
-    }
-
-    public void setY(double value) {
-        yCoord = value;
-    }
+    
+  
 
     public void setZ(double value) {
         zCoord = value;
     }
-    public boolean compare(double xc,double yc,double zc){//Метод сравнения двух точек
-        if (xc == yc)
-            return false;
-        else if (xc == zc)
-            return false;
-        else if (yc == zc)
+    public static boolean compare(Point3d coord1, Point3d coord2){//Метод сравнения двух точек
+        if (coord1 == coord2)
             return false;
         else
             return true;
